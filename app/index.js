ff4d0f248bcbf50edf00c1a3b46826935ed952b8
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-04 08:13:35 +0530

    Update index.js
    Removed initial comments

--- app/index.js
+++ app/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css'
-// component
-// state
-// lifecycle
-// ui
 
 class App extends React.Component{
     render() {
         return (
             <div>
-                Hello nandini!
+                Hello World!
             </div>
         ) 
     }
 }
 
 ReactDOM.render(
-    //reactDom needs to argument as below
-    // 1. React.element
-    // 2. where to render to the element to
     <App/>,
     document.getElementById('app')
 )
