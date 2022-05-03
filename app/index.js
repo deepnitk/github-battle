8ff80656d71725132bfbd3158ea82730e08aaa4b
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-03 14:15:49 +0530

    Added dark/light Theme Provider using Context
    Added dark/light Theme Provider using Context

--- app/index.js
+++ app/index.js
@@ -1,36 +1,40 @@
-import React from "react";
-import ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import './index.css'
-import Popular from "./components/Popular";
-import Battle from "./components/Battle";
-import { ThemeConsumer, ThemeProvider} from './contexts/theme'
+import Popular from './components/Popular'
+import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            theme: 'light',
-            toggleTheme: () => {
-                this.setState(({ theme }) => ({
-                    theme: theme === 'light' ? 'dark': 'light'
-                }))
-            }
-        }
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
     }
-    render() {
-        return (
-            <ThemeProvider value={}>
-                <div className="container">
-                    <Battle/>
-                </div>
-            </ThemeProvider>
-        ) 
-    }
+  }
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+
+            <Popular />
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
